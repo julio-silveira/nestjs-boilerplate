73afed762959033ad80ddfcd2a772ae4806c1429
julio-silveira <julioqueiroz15@gmail.com>
2024-07-06 17:27:39 -0300

    feature: add userDataBuilder method

--- src/users/domain/entities/user.entity.spec.ts
+++ src/users/domain/entities/user.entity.spec.ts
@@ -1,16 +1,11 @@
-import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from './user.entity';
+import { UserDataBuilder } from '../testing/user-data-builder';
 
 describe('UserEntity', () => {
   let props: UserProps;
   let entity: UserEntity;
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-
+    props = UserDataBuilder();
     entity = new UserEntity(props);
   });
 
@@ -19,5 +14,28 @@ describe('UserEntity', () => {
     expect(entity.props.email).toBe(props.email);
     expect(entity.props.password).toBe(props.password);
     expect(entity.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('getter name', () => {
+    expect(entity.name).toBeDefined();
+    expect(entity.name).toBe(props.name);
+    expect(typeof entity.name).toBe('string');
+  });
+
+  it('getter email', () => {
+    expect(entity.email).toBeDefined();
+    expect(entity.email).toBe(props.email);
+    expect(typeof entity.email).toBe('string');
+  });
+
+  it('getter password', () => {
+    expect(entity.password).toBeDefined();
+    expect(entity.password).toBe(props.password);
+    expect(typeof entity.password).toBe('string');
+  });
+
+  it('getter createdAt', () => {
+    expect(entity.createdAt).toBeDefined();
+    expect(entity.createdAt).toBeInstanceOf(Date);
   });
 });
