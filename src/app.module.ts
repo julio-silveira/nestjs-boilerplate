9ca66ccfceb6bc96935d24528a124fb69bf915ad
julio-silveira <julioqueiroz15@gmail.com>
2024-07-06 17:07:02 -0300

    feature: creating users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './shared/infrastructure/config/config.module';
+import { UsersModule } from './users/infrastructure/users.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
